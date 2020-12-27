6bf6c95fb99c3198786d0dfd323d47143e059c9a
Zachary Martinez <martinez.zach.m@gmail.com>
2020-12-27 15:19:40 -0800

    Add functions that control data model and storage

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-alert("Ba-wip gra-na wip ni-ni-bong!")
+let myLibrary = []
+
+function Book({title, author, pages, read}) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read || false
+}
+
+function addBookToLibrary(record) {
+    let newBook = new Book(record)
+    myLibrary.push(newBook)
+}
+
+function addBooksToLibrary(array) {
+    array.forEach(record => {
+        addBookToLibrary(record)
+    })
+}
+
+function removeBookFromLibrary(i) {
+    myLibrary.splice(i, 1)
+}
+
+function changeBookInLibrary(i, newRecord) {
+    let book = myLibrary[i]
+    let keys = Object.keys(book)
+    keys.forEach(key => { // Set the value of all properties in book to match newRecord
+        book[key] = newRecord[key] || book[key]
+    })
+}
+
+function saveLibrary() {
+    try {
+        let dataString = JSON.stringify(myLibrary)
+        localStorage['myLibrary'] = dataString
+        console.log(`Saved ${myLibrary.length} books`)
+    } catch (error) {
+        console.log(`Unable to write to localStorage`)
+        console.log(error);
+    }
+}
+
+function loadLibrary() {
+    try {
+        let dataString = localStorage['myLibrary']
+        let data = JSON.parse(dataString)
+        addBooksToLibrary(data)
+        console.log(`Loaded ${data.length} books`)
+    } catch (error) {
+        console.log(`Cannot read from localStorage`)
+        console.log(error)
+    }
+}
+
+function clearLibrary() {
+    if (confirm("Do you want to delete all your books?")) {
+        let numberOfBooks = myLibrary.length
+        myLibrary = []
+        localStorage['myLibrary'] = '[]'
+        console.log(`Deleted ${numberOfBooks} books`)
+    }
+}
+
+function createTestBooks() {
+    const testBooks = [
+        {"title":"War and Peace","author":"Tolstoy","pages":1400,"read":false},
+        {"title":"20,000 Leagues Under The Sea","author":"Jules Verne","pages":504,"read":false},
+        {"title":"Around The World in 80 Days","author":"Jules Verne","pages":200,"read":false}
+    ]
+
+    addBooksToLibrary(testBooks)
+}
